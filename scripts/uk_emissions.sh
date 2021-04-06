4e7b615e67ca02acf8d9569b11b58469ea93cb63
moryama171 <55793984+moryama171@users.noreply.github.com>
2021-04-06 20:08:25 +0200

    Commente ek_emissions.sh

--- scripts/uk_emissions.sh
+++ scripts/uk_emissions.sh
@@ -2,6 +2,7 @@
 
 TIME1=`date +%Y-%m-%d_%H-%M-%S`
 
+# Sanity check
 echo "Current working directory: `pwd`"
 echo "Starting run at: " ${TIME1}
 
@@ -13,6 +14,7 @@ echo " "
 
 cd /home/lauragf/Dropbox/RC/Performance_Monitor/
 
+# Create python environment to make sure we woek on same version
 source Environments/perfenv/bin/activate
 
 #pip install -r requirements.txt
@@ -21,6 +23,7 @@ echo "HI"
 
 echo "Current working directory: `pwd`"
 
+# Execute
 /home/lauragf/anaconda3/bin/python3.8 performance_monitor.py
 
 deactivate
